cf12d6b3614c9b9a266d1efe7055e5dc17783ac7
nikhil <boorsunikhil2001@example.com>
2026-04-09 16:26:13 +0530

    fix: wildcard route for express v5

--- backend/src/index.js
+++ backend/src/index.js
@@ -46,9 +46,9 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-  });
+ app.get("/*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
 }
 
 server.listen(PORT, () => {
